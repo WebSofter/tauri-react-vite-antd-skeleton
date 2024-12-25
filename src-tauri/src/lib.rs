5e43775c0b2abdfeb5388226b341c0a196ae40d1
David Amirkhanov <mail.websofter@gmail.com>
2024-12-25 09:57:56 +0300

    init

---
+++ src-tauri/src/lib.rs
@@ -0,0 +1,26 @@
+use tauri::tray::TrayIconBuilder;
+// Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
+#[tauri::command]
+fn greet(name: &str) -> String {
+    format!("Hello, {}! You've been greeted from Rust!", name)
+}
+
+#[cfg_attr(mobile, tauri::mobile_entry_point)]
+pub fn run() {
+    tauri::Builder::default()
+        .plugin(tauri_plugin_opener::init())
+        .plugin(tauri_plugin_positioner::init())
+        // This is required to get tray-relative positions to work
+        .setup(|app| {
+            // note that this will create a new TrayIcon
+            TrayIconBuilder::new()
+                .on_tray_icon_event(|app, event| {
+                    tauri_plugin_positioner::on_tray_event(app.app_handle(), &event);
+                })
+                .build(app)?;
+            Ok(())
+        })
+        .invoke_handler(tauri::generate_handler![greet])
+        .run(tauri::generate_context!())
+        .expect("error while running tauri application");
+}
