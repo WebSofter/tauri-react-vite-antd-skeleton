7d6baab0c9f86e709fdfa2c1929d44a19e1d7c51
David Amirkhanov <mail.websofter@gmail.com>
2024-12-25 11:28:18 +0300

    init

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,37 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App";
+import { ConfigProvider } from 'antd';
+import { antdConfig } from '@/constants';
+import { Provider } from 'react-redux';
+import { persistStore } from 'redux-persist';
+import { PersistGate } from 'redux-persist/integration/react';
+import Loader from '@/components/loader';
+import { store } from '@/store';
+import { injectStore } from '@/lib/http';
+import App from '@/App';
+import '@/index.css';
 
-ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
+// ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>,
+// );
+
+const persistor = persistStore(store);
+injectStore(store);
+
+ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+    <ConfigProvider {...antdConfig}>
+      <Provider store={store}>
+        <PersistGate loading={<Loader />} persistor={persistor}>
+          <App />
+        </PersistGate>
+      </Provider>
+    </ConfigProvider>
+  </React.StrictMode>
 );
+
+window?.addEventListener('vite:preloadError', () => {
+  window?.location?.reload();
+});
